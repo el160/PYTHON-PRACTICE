0d280075c518ea41b7daa6ee6f9cc022ec9deead
el160 <ljhongeri04@gmail.com>
2024-11-27 15:26:20 +0300

    code

--- OOP/inheritance .py
+++ OOP/inheritance .py
@@ -8,13 +8,27 @@ class Vehicle:
         self.year = year
         
 #creating a method
-def description(self):
-    print(self.color,self.brand,self.year)
+    def description(self):
+     print(f'color:{self.color},brand:{self.brand},year:{self.year}')
     
 #creating an object
-car = Vehicle('red','Toyota',2015)
+car = Vehicle('red','BMW',2015)
 #calling the method
 car.description()
 
 #creating a child class
-
+class Bike(Vehicle):
+    def __init__(self, color, brand, year):
+        super().__init__(color, brand, year)# super method makes sure that the parent class properties are called and not overrided by child class properties
+        self.wheels = 2 #adding a new property to the child class thats not in parent class
+        
+#adding new method to bike
+    def feature(self):
+     print(f'this bike has {self.wheels} wheels')
+    
+#creating and oblect of child class
+cycle = Bike('white','Honda',2019)
+#calling the method
+cycle.description()
+cycle.feature()
+        
